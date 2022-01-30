fdf200af8918381ff48f0f1bfb9362d816123766
Aparna <tiwari.aparna2899@gmail.com>
2022-01-30 22:39:08 +0530

    Add methods to Array.prototype

--- block-BJaaih/code/index.js
+++ block-BJaaih/code/index.js
@@ -3,8 +3,14 @@
 `myMap` will behave similar to how the `Array.map` works. To test the implementation use the code below.
 */
 
-// myMap function goes here
 
+Array.prototype.myMap = function (cb) {
+  let final = [];
+  for (let i = 0; i < this.length; i++) {
+    final.push(cb(this[i]));
+  }
+  return final;
+}
 // Test the myMap function you created above
 
 let numbers = [1, 5, 6, 8, 9];
@@ -20,15 +26,25 @@ let capitalWords = words
     return word.charAt(0).toUpperCase() + word.slice(1);
   })
   .join(' ');
+
+console.group('1')
 console.log(doubleNum); // it should be [1, 9, 11, 15, 17]
 console.log(capitalWords); // it should be 'Quick Brown Fox Jumped Over A Lazy Dog'
+console.groupEnd()
 
 /*
 2. Add a method named `myFilter` to Array.prototype. myFilter should behave similar to Array.filter.
 After adding the function test it using the code below.
 */
 
-// You code goes here
+Array.prototype.myFilter = function (cb) {
+  let final = [];
+  for (let i = 0; i < this.length; i++) {
+    if (cb(this[i])) 
+      final.push(this[i]);
+  }
+  return final;
+}
 
 let even = numbers.myFilter(function (num) {
   return num % 2 === 0;
@@ -39,8 +55,11 @@ let filteredWords = words
     return word.length > 3;
   })
   .join(' ');
+
+console.group('2')
 console.log(even); // it should be [6, 8]
 console.log(filteredWords); // it should be 'quick brown jumped over lazy';
+console.groupEnd()
 
 /*
 3. Add a method named `shuffle` to Array.prototype. Using the method shuffle will return a
@@ -49,13 +68,21 @@ Make sure it does not the changes the original array.
 
 */
 
-// You code goes here
+Array.prototype.shuffle = function () {
+  let final = [];
+  for (let i = 0; i < this.length; i++) { 
+    final.push(Math.floor(Math.random()*this.length));
+  }
+  return final;
+}
 
 // Test to check the shuffle method (It will return different output every time you call)
+console.group('3')
 console.log(numbers.shuffle());
 console.log(numbers.shuffle());
 console.log(words.shuffle());
 console.log(words.shuffle());
+console.groupEnd()
 
 /*
 4. Add a method named `unique` to Array.prototype. Using the method will return a
@@ -63,25 +90,45 @@ new array and it should only contain unique elements in the array.
 Unique means no element should come multiple times.
 */
 
-// You code goes here
+Array.prototype.unique = function () {
+  let final = [];
+  for (let i = 0; i < this.length; i++) {
+    if(!final.includes(this[i]))
+    final.push(this[i])
+  }
+  return final;
+}
 
 // Test to check the shuffle method (It will return different output every time you call)
 let num = [1, 2, 3, 4, 2, 3, 6, 7, 7];
 let strings = 'helloworld'.split('');
 
+console.group('4')
 console.log(num.unique()); // [1, 2, 3, 4, 6, 7]
 console.log(strings.unique()); // ['h', 'e', 'l', 'o', 'w', 'r', 'd']
+console.groupEnd()
 
 /*
 5. Add a method named `intersection` to Array.prototype. The method intersection will accept an array and returns a new
 array that will contain only element that is common in both the array.
 */
 
-// You code goes here
+Array.prototype.intersection = function (arr) {
+  let final = [];
+  for (let i = 0; i < this.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if((this[i] === arr[j]) && (!final.includes(this[i])))
+      final.push(arr[j]);
+    }
+  }
+  return final;
+}
 
 // Test to check the shuffle method (It will return different output every time you call)
+console.group('5')
 console.log(num.intersection([2, 7, 11, 32])); // [2, 7]
 console.log(strings.intersection('heyworld'.split(''))); // ['h', 'e', 'o', 'w', 'r', 'l', 'd']
+console.groupEnd()
 
 /*
 6. Add a method named `chunk` to Array.prototype. The method chunk will accept one parameter `length`
@@ -89,9 +136,17 @@ and split the array into groups the length of size. If array can't be split even
 chunk will be the remaining elements. `length` should default to 1.
 */
 
-// You code goes here
-
+Array.prototype.chunk = function (length) {
+  let final = [];
+  while(Boolean(this.length)) {
+    final.push(this.splice(0,length));
+  }  
+  return final;
+}
 // Test to check the shuffle method (It will return different output every time you call)
+console.group('6')
 console.log(num.chunk(2)); // [[1, 2], [3, 4], [2, 3], [6, 7], [7]]
 console.log(num.chunk()); // [[1], [2], [3], [4], [2], [3], [6], [7], [7]]
 console.log(strings.chunk(3)); // [['h', 'e', 'l'], ['l', 'o', 'w'], ['o', 'r', 'l'], ['d']]
+console.groupEnd()
+
